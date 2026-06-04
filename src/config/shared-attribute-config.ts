3e9c52afbd89f8fdb0354e45080d0426d7f79938
Sagi Carmel <sagica5273@gmail.com>
2026-06-04 15:54:20 +0300

    chore: allow to widen sharred attributes so they won't be forced

--- src/config/shared-attribute-config.ts
+++ src/config/shared-attribute-config.ts
@@ -3,6 +3,20 @@ export type SharedAttributeConfig = Record<
   string | number | boolean | undefined
 >;
 
+type WidenPrimitive<V> = V extends boolean
+  ? boolean
+  : V extends number
+    ? number
+    : V extends string
+      ? string
+      : V;
+
+export type ProcessedSharedConfig<T> = {
+  [K in keyof T]?: WidenPrimitive<T[K]>;
+};
+
 export const sharedAttributeConfig = <const T extends SharedAttributeConfig>(
-  config: T,
-) => config;
+  config: T & ProcessedSharedConfig<T>,
+): ProcessedSharedConfig<T> => {
+  return config;
+};
