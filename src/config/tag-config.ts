449fac56c7f416e8c6bbca972e7ec44db9a72548
Sagi Carmel <sagica5273@gmail.com>
2026-06-09 15:33:42 +0300

    chore: modification for the tag config

--- src/config/tag-config.ts
+++ src/config/tag-config.ts
@@ -2,15 +2,12 @@ import type { Keyof } from "../types.ts";
 import type { PrimissiveAttributeType } from "./attribute-config.ts";
 
 export type PremissivePrimitiveTag = string;
-export type PermissiveInnerHTMLTagDefinition<
-  PossibleTags extends string = string,
-> = "*" | (PossibleTags | "#text")[];
 
 export type PremissiveTagDefinition<Tags extends string = string> = Record<
   Tags,
   {
     attributes?: Record<string, PrimissiveAttributeType>;
-    innerHTML: PermissiveInnerHTMLTagDefinition<Tags>;
+    innerHTML: "*" | (Tags | "#text")[];
   }
 >;
 
