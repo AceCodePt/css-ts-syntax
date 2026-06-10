65323ad4c6da0e46a36bc4127f9d138732b8dd6c
Sagi Carmel <sagica5273@gmail.com>
2026-06-10 10:08:45 +0300

    feat: css syntax config works perfectly

--- src/config/css-syntax-config.ts
+++ src/config/css-syntax-config.ts
@@ -2,21 +2,22 @@ export type PrimitiveSyntaxValue = string;
 
 declare const _error: unique symbol;
 
-export type SyntaxKeysConfig = Record<string, unknown>;
+export type SyntaxKeysConfig = Record<`<${string}${string}>`, string>;
 
-export type ValidatedSyntaxKeys<T extends SyntaxKeysConfig> = {
-  [K in keyof T]: K extends string
-    ? T[K] extends string
+export type ValidatedSyntaxKeys<T extends SyntaxKeysConfig = SyntaxKeysConfig> =
+  {
+    [K in keyof T]: K extends string
       ? K extends `<${string}${string}>`
-        ? T[K]
+        ? T[K] extends string
+          ? T[K]
+          : T[K] & {
+              [_error]: `🛑 ERROR: The value of '${K}' must be any type of string`;
+            }
         : T[K] & {
             [_error]: `🛑 ERROR: The key '${K}' must be wrapped in angle brackets (e.g., '<${K}>')`;
           }
-      : T[K] & {
-          [_error]: `🛑 ERROR: The value of '${K}' must be any type of string`;
-        }
-    : T[K];
-};
+      : T[K];
+  };
 
 export function cssSyntaxConfig<const T extends SyntaxKeysConfig>(
   config: ValidatedSyntaxKeys<T>,
