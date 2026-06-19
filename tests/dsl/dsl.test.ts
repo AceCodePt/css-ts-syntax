d4de51c78e7e5e7165f3053d92f6d8387edf2942
Sagi Carmel <sagica5273@gmail.com>
2026-06-19 19:04:55 +0300

    chore: dsl test

--- tests/dsl/dsl.test.ts
+++ tests/dsl/dsl.test.ts
@@ -4,38 +4,71 @@ import { assertType, type Equal } from "../type-utils.ts";
 import assert from "node:assert";
 
 describe("Primitive types", () => {
-  test("'string' of DSL parses a primitive string", () => {
-    assert.strictEqual(parseValueAgainstDSL("string", ""), "");
-    dslString("string");
-    assertType<Equal<DSLInfer<"string">, string>>();
+  describe("string", () => {
+    test("Type Infrence", () => {
+      assertType<Equal<DSLInfer<"string">, string>>();
+    });
+    test("Runtime Validation", () => {
+      dslString("string");
+    });
+    test("Parse", () => {
+      assert.strictEqual(parseValueAgainstDSL("string", ""), "");
+    });
   });
 
-  test("'number' of DSL parses a primitive number", () => {
-    assert.strictEqual(parseValueAgainstDSL("number", 0), 0);
-    dslString("number");
-    assertType<Equal<DSLInfer<"number">, number>>();
+  describe("number", () => {
+    test("Type Infrence", () => {
+      assertType<Equal<DSLInfer<"number">, number>>();
+    });
+    test("Runtime Validation", () => {
+      dslString("number");
+    });
+    test("Parse", () => {
+      assert.strictEqual(parseValueAgainstDSL("number", 0), 0);
+    });
   });
 
-  test("'bigint' of DSL parses a primitive bigint", () => {
-    assert.strictEqual(
-      parseValueAgainstDSL("bigint", BigInt("1")),
-      BigInt("1"),
-    );
-    dslString("bigint");
-    assertType<Equal<DSLInfer<"bigint">, bigint>>();
+  describe("bigint", () => {
+    test("Type Infrence", () => {
+      assertType<Equal<DSLInfer<"bigint">, bigint>>();
+    });
+    test("Runtime Validation", () => {
+      dslString("bigint");
+    });
+    test("Parse", () => {
+      assert.strictEqual(
+        parseValueAgainstDSL("bigint", BigInt("1")),
+        BigInt("1"),
+      );
+    });
   });
 
-  test("'boolean' of DSL parses a primitive boolean", () => {
-    assert.strictEqual(parseValueAgainstDSL("boolean", true), true);
-    assert.strictEqual(parseValueAgainstDSL("boolean", false), false);
-    dslString("boolean");
-    assertType<Equal<DSLInfer<"boolean">, boolean>>();
+  describe("boolean", () => {
+    test("Type Infrence", () => {
+      assertType<Equal<DSLInfer<"boolean">, boolean>>();
+    });
+    test("Runtime Validation", () => {
+      dslString("boolean");
+    });
+    test("Parse", () => {
+      assert.strictEqual(parseValueAgainstDSL("boolean", true), true);
+      assert.strictEqual(parseValueAgainstDSL("boolean", false), false);
+    });
   });
 
-  test("'undefined' of DSL parses a primitive undefined", () => {
-    assert.strictEqual(parseValueAgainstDSL("undefined", undefined), undefined);
-    dslString("undefined");
-    assertType<Equal<DSLInfer<"undefined">, undefined>>();
+  describe("undefined", () => {
+    test("Type Infrence", () => {
+      assertType<Equal<DSLInfer<"undefined">, undefined>>();
+    });
+    test("Runtime Validation", () => {
+      dslString("undefined");
+    });
+    test("Parse", () => {
+      assert.strictEqual(
+        parseValueAgainstDSL("undefined", undefined),
+        undefined,
+      );
+    });
   });
 
   test("returns the same reference for objects-free primitives", () => {
@@ -45,110 +78,279 @@ describe("Primitive types", () => {
   });
 });
 
-describe("Literal values", () => {
-  test("'true' of DSL parses a literal true", () => {
-    assert.strictEqual(parseValueAgainstDSL("true", true), true);
-    dslString("true");
-    assertType<Equal<DSLInfer<"true">, true>>();
+describe("Literals", () => {
+  describe("Literal Boolean", () => {
+    describe("true", () => {
+      test("Type Infrence", () => {
+        assertType<Equal<DSLInfer<"true">, true>>();
+      });
+      test("Runtime Validation", () => {
+        dslString("true");
+      });
+      test("Parse", () => {
+        assert.strictEqual(parseValueAgainstDSL("true", true), true);
+      });
+    });
+
+    describe("false", () => {
+      test("Type Infrence", () => {
+        assertType<Equal<DSLInfer<"false">, false>>();
+      });
+      test("Runtime Validation", () => {
+        dslString("false");
+      });
+      test("Parse", () => {
+        assert.strictEqual(parseValueAgainstDSL("false", false), false);
+      });
+    });
   });
 
-  test("'false' of DSL parses a literal false", () => {
-    assert.strictEqual(parseValueAgainstDSL("false", false), false);
-    dslString("false");
-    assertType<Equal<DSLInfer<"false">, false>>();
+  describe("Literal Number", () => {
+    describe("0, 1", () => {
+      test("Type Infrence", () => {
+        assertType<Equal<DSLInfer<"0">, 0>>();
+        assertType<Equal<DSLInfer<"1">, 1>>();
+      });
+      test("Runtime Validation", () => {
+        dslString("0");
+        dslString("1");
+      });
+      test("Parse", () => {
+        assert.strictEqual(parseValueAgainstDSL("0", 0), 0);
+        assert.strictEqual(parseValueAgainstDSL("1", 1), 1);
+      });
+    });
   });
 
-  test("0 of DSL parses a literal 0", () => {
-    assert.strictEqual(parseValueAgainstDSL("0", 0), 0);
-    dslString("0");
-    assertType<Equal<DSLInfer<"0">, 0>>();
+  describe("Literal Strings", () => {
+    describe("Single Quote ('')", () => {
+      test("Type Infrence", () => {
+        assertType<Equal<DSLInfer<"''">, "">>();
+        assertType<Equal<DSLInfer<"'a'">, "a">>();
+      });
+      test("Runtime Validation", () => {
+        dslString("''");
+        dslString("'a'");
+      });
+      test("Parse", () => {
+        assert.strictEqual(parseValueAgainstDSL("''", ""), "");
+        assert.strictEqual(parseValueAgainstDSL("'a'", "a"), "a");
+      });
+    });
+
+    describe('Double Quote ("")', () => {
+      test("Type Infrence", () => {
+        assertType<Equal<DSLInfer<'""'>, "">>();
+        assertType<Equal<DSLInfer<'"a"'>, "a">>();
+      });
+      test("Runtime Validation", () => {
+        dslString('""');
+        dslString('"a"');
+      });
+      test("Parse", () => {
+        assert.strictEqual(parseValueAgainstDSL('""', ""), "");
+        assert.strictEqual(parseValueAgainstDSL('"a"', "a"), "a");
+      });
+    });
+
+    describe("Template Literal (``)", () => {
+      test("Type Infrence", () => {
+        assertType<Equal<DSLInfer<"``">, ``>>();
+        assertType<Equal<DSLInfer<"`a`">, "a">>();
+      });
+      test("Runtime Validation", () => {
+        dslString("``");
+        dslString("`a`");
+      });
+      test("Parse", () => {
+        assert.strictEqual(parseValueAgainstDSL("``", ``), ``);
+        assert.strictEqual(parseValueAgainstDSL("`a`", "a"), "a");
+      });
+    });
+  });
+});
+
+describe("Literal String with pipe - '|' \"|\" `|` [EDGE CASE]", () => {
+  describe("Single Quote ('|')", () => {
+    test("Type Infrence", () => {
+      assertType<Equal<DSLInfer<"'|'">, "|">>();
+    });
+    test("Runtime Validation", () => {
+      dslString("'|'");
+    });
+    test("Parse", () => {
+      assert.strictEqual(parseValueAgainstDSL("'|'", "|"), "|");
+    });
   });
 
-  test("10 of DSL parses a literal 10", () => {
-    assert.strictEqual(parseValueAgainstDSL("10", 10), 10);
-    dslString("10");
-    assertType<Equal<DSLInfer<"10">, 10>>();
+  describe('Double Quote ("|")', () => {
+    test("Type Infrence", () => {
+      assertType<Equal<DSLInfer<'"|"'>, "|">>();
+    });
+    test("Runtime Validation", () => {
+      dslString('"|"');
+    });
+    test("Parse", () => {
+      assert.strictEqual(parseValueAgainstDSL('"|"', "|"), "|");
+    });
+  });
+
+  describe("Template Literal (`|`)", () => {
+    test("Type Infrence", () => {
+      assertType<Equal<DSLInfer<"`|`">, `|`>>();
+    });
+    test("Runtime Validation", () => {
+      dslString("`|`");
+    });
+    test("Parse", () => {
+      assert.strictEqual(parseValueAgainstDSL("`|`", `|`), `|`);
+    });
+  });
+});
+
+describe("Union Type (|)", () => {
+  describe("Union Type with Primitives - string | number | bigint | boolean | undefined", () => {
+    test("Type Infrence", () => {
+      assertType<
+        Equal<
+          DSLInfer<"string | number | bigint | boolean | undefined">,
+          string | number | bigint | boolean | undefined
+        >
+      >();
+    });
+    test("Runtime Validation", () => {
+      dslString("string | number | bigint | boolean | undefined");
+    });
+    test("Parse", () => {
+      assert.strictEqual(
+        parseValueAgainstDSL(
+          "string | number | bigint | boolean | undefined",
+          "",
+        ),
+        "",
+      );
+
+      assert.strictEqual(
+        parseValueAgainstDSL(
+          "string | number | bigint | boolean | undefined",
+          0,
+        ),
+        0,
+      );
+
+      assert.strictEqual(
+        parseValueAgainstDSL(
+          "string | number | bigint | boolean | undefined",
+          BigInt("0"),
+        ),
+        BigInt("0"),
+      );
+
+      assert.strictEqual(
+        parseValueAgainstDSL(
+          "string | number | bigint | boolean | undefined",
+          true,
+        ),
+        true,
+      );
+
+      assert.strictEqual(
+        parseValueAgainstDSL(
+          "string | number | bigint | boolean | undefined",
+          undefined,
+        ),
+        undefined,
+      );
+    });
+  });
+
+  describe("Union Type with Literal Value", () => {
+    describe("Boolean Literal", () => {
+      test("Type Infrence", () => {
+        assertType<Equal<DSLInfer<"true | false">, true | false>>();
+      });
+      test("Runtime Validation", () => {
+        dslString("true | false");
+      });
+      test("Parse", () => {
+        assert.strictEqual(parseValueAgainstDSL("true | false", true), true);
+        assert.strictEqual(parseValueAgainstDSL("true | false", false), false);
+      });
+    });
+
+    describe("Number Literal", () => {
+      test("Type Infrence", () => {
+        assertType<Equal<DSLInfer<"0 | 1">, 0 | 1>>();
+      });
+      test("Runtime Validation", () => {
+        dslString("0 | 1");
+      });
+      test("Parse", () => {
+        assert.strictEqual(parseValueAgainstDSL("0 | 1", 0), 0);
+        assert.strictEqual(parseValueAgainstDSL("0 | 1", 1), 1);
+      });
+    });
+
+    describe("String Literal", () => {
+      test("Type Infrence", () => {
+        assertType<Equal<DSLInfer<"'foo' | 'bar'">, "foo" | "bar">>();
+      });
+      test("Runtime Validation", () => {
+        dslString("'foo' | 'bar'");
+      });
+      test("Parse", () => {
+        assert.strictEqual(parseValueAgainstDSL("'foo' | 'bar'", "bar"), "bar");
+        assert.strictEqual(parseValueAgainstDSL("'foo' | 'bar'", "bar"), "bar");
+      });
+    });
+  });
+
+  describe("Complex union - true | 0 | 'a' | `b` | undefined | \"c\"", () => {
+    test("Type Infrence", () => {
+      assertType<
+        Equal<
+          DSLInfer<"true | 0 | 'a' | `b` | undefined | \"c\"">,
+          true | 0 | "a" | `b` | undefined | "c"
+        >
+      >();
+    });
+    test("Runtime Validation", () => {
+      dslString("true | 0 | 'a' | `b` | undefined | \"c\"");
+    });
+    test("Parse", () => {
+      assert.strictEqual(
+        parseValueAgainstDSL("true | 0 | 'a' | `b` | undefined | \"c\"", true),
+        true,
+      );
+      assert.strictEqual(
+        parseValueAgainstDSL("true | 0 | 'a' | `b` | undefined | \"c\"", 0),
+        0,
+      );
+      assert.strictEqual(
+        parseValueAgainstDSL("true | 0 | 'a' | `b` | undefined | \"c\"", "a"),
+        "a",
+      );
+      assert.strictEqual(
+        parseValueAgainstDSL("true | 0 | 'a' | `b` | undefined | \"c\"", "b"),
+        "b",
+      );
+      assert.strictEqual(
+        parseValueAgainstDSL(
+          "true | 0 | 'a' | `b` | undefined | \"c\"",
+          undefined,
+        ),
+        undefined,
+      );
+      assert.strictEqual(
+        parseValueAgainstDSL("true | 0 | 'a' | `b` | undefined | \"c\"", "c"),
+        "c",
+      );
+    });
   });
 });
 
 describe("Quoted string literals", () => {
-  describe("single quotes ''", () => {
-    test("'' of DSL parses a literal ''", () => {
-      assert.strictEqual(parseValueAgainstDSL("''", ""), "");
-      dslString("''");
-      assertType<Equal<DSLInfer<"''">, "">>();
-    });
-
-    test("'a' of DSL parses a literal 'a'", () => {
-      assert.strictEqual(parseValueAgainstDSL("'a'", "a"), "a");
-      dslString("'a'");
-      assertType<Equal<DSLInfer<"'a'">, "a">>();
-    });
-
-    test("'|' of DSL parses a literal '|'", () => {
-      assert.strictEqual(parseValueAgainstDSL("'|'", "|"), "|");
-      dslString("'|'");
-      assertType<Equal<DSLInfer<"'|'">, "|">>();
-    });
-
-    test("`'|'` of DSL parses a literal `'|'`", () => {
-      assert.strictEqual(parseValueAgainstDSL("`'|'`", "'|'"), "'|'");
-      dslString("`'|'`");
-      assertType<Equal<DSLInfer<"`'|'`">, "'|'">>();
-    });
-  });
-
-  describe('double quotes ""', () => {
-    test('"" of DSL parses a literal ""', () => {
-      assert.strictEqual(parseValueAgainstDSL('""', ""), "");
-      dslString('""');
-      assertType<Equal<DSLInfer<'""'>, "">>();
-    });
-
-    test('"a" of DSL parses a literal "a"', () => {
-      assert.strictEqual(parseValueAgainstDSL('"a"', "a"), "a");
-      dslString('"a"');
-      assertType<Equal<DSLInfer<'"a"'>, "a">>();
-    });
-
-    test('"|" of DSL parses a literal "|"', () => {
-      assert.strictEqual(parseValueAgainstDSL('"|"', "|"), "|");
-      dslString('"|"');
-      assertType<Equal<DSLInfer<'"|"'>, "|">>();
-    });
-
-    test('`"|"` of DSL parses a literal `"|"`', () => {
-      assert.strictEqual(parseValueAgainstDSL('`"|"`', '"|"'), '"|"');
-      dslString('`"|"`');
-      assertType<Equal<DSLInfer<'`"|"`'>, '"|"'>>();
-    });
-  });
-
   describe("backticks ``", () => {
-    test("`` of DSL parses a literal ``", () => {
-      assert.strictEqual(parseValueAgainstDSL("``", ""), "");
-      dslString("``");
-      assertType<Equal<DSLInfer<"``">, "">>();
-    });
-
-    test("`a` of DSL parses a literal `a`", () => {
-      assert.strictEqual(parseValueAgainstDSL("`a`", "a"), "a");
-      dslString("`a`");
-      assertType<Equal<DSLInfer<"`a`">, "a">>();
-    });
-
-    test("`|` of DSL parses a literal `|`", () => {
-      assert.strictEqual(parseValueAgainstDSL("`|`", "|"), "|");
-      dslString("`|`");
-      assertType<Equal<DSLInfer<"`|`">, "|">>();
-    });
-
-    test("'`|`' of DSL parses a literal '`|`'", () => {
-      assert.strictEqual(parseValueAgainstDSL("'`|`'", "`|`"), "`|`");
-      dslString("'`|`'");
-      assertType<Equal<DSLInfer<"'`|`'">, "`|`">>();
-    });
-
     test("`${number}${'%'}` of DSL parses a literal '1%'", () => {
       assert.strictEqual(parseValueAgainstDSL("`${number}${'%'}`", "1%"), "1%");
       dslString("`${number}${'%'}`");
