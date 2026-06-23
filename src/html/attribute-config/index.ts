f42b09f2a9a9442e1e925aba685d2b589e4a0f77
Sagi Carmel <sagica5273@gmail.com>
2026-06-23 10:49:44 +0300

    feat: attribute-config

--- src/html/attribute-config/index.ts
+++ src/html/attribute-config/index.ts
@@ -1,10 +1,18 @@
+import { dslString } from "@/dsl/index.ts";
 import type {
   BaseHTMLAttributeConfig,
   ValidateHTMLAttributeConfig,
 } from "./types.ts";
 
-export const htmlAttributeConfig = <const A extends BaseHTMLAttributeConfig>(
-  config: ValidateHTMLAttributeConfig<A>,
+export const htmlAttributeConfig = <
+  const Keywords extends Record<string, any>,
+  const A extends BaseHTMLAttributeConfig,
+>(
+  supportedKeywords: Keywords,
+  config: ValidateHTMLAttributeConfig<Keywords, A>,
 ) => {
+  for (const key in config) {
+    dslString(supportedKeywords, config[key]);
+  }
   return config as A;
 };
--- src/html/attribute-config/types.ts
+++ src/html/attribute-config/types.ts
@@ -1,8 +1,18 @@
-import type { DSLValidate } from "@/dsl/index.ts";
+import type { DSLInfer, DSLValidate } from "@/dsl/index.ts";
 
 export type BaseHTMLAttributeValue = string;
 export type BaseHTMLAttributeConfig = Record<string, BaseHTMLAttributeValue>;
 
-export type ValidateHTMLAttributeConfig<A extends BaseHTMLAttributeConfig> = {
-  [K in keyof A]: K extends string ? DSLValidate<A[K]> : A[K];
+export type ValidateHTMLAttributeConfig<
+  Keywords extends Record<string, any>,
+  A extends BaseHTMLAttributeConfig,
+> = {
+  [K in keyof A]: K extends string ? DSLValidate<Keywords, A[K]> : A[K];
+};
+
+export type InferHTMLAttributeConfig<
+  Keywords extends Record<string, any>,
+  A extends BaseHTMLAttributeConfig,
+> = {
+  [K in keyof A]: K extends string ? DSLInfer<Keywords, A[K]> : A[K];
 };
