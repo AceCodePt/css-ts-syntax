2d66144556c5cb6b711f0f069edf1fa950728a23
Sagi Carmel <sagica5273@gmail.com>
2026-06-26 15:01:07 +0300

    feat: new create component

--- src/create-component.ts
+++ src/create-component.ts
@@ -1,112 +1,166 @@
-import type { DSLInfer } from "@/dsl/index.ts";
-import type { BaseHTMLAttributeConfig } from "@/html/attribute-config/types.ts";
-import type { BaseHTMLTagConfig } from "@/html/tag-config/types.ts";
-import type { Keyof, MakeUndefinedOptional } from "@/types.ts";
+import type { BaseCSSAttributesConfig } from "./css/attribute-config/types.ts";
+import type { BaseCSSPropertiesConfig } from "./css/properties-config/types.ts";
+import type { BaseCSSSyntaxConfig } from "./css/syntax-config/types.ts";
+import type { DSLInfer, SupportedKeywords } from "./dsl/index.ts";
+import type {
+  BaseHTMLAttributesConfig,
+  InferHTMLAttributesConfig,
+} from "./html/attribute-config/types.ts";
+import type { BaseHTMLTagConfig } from "./html/tag-config/types.ts";
+import type { Keyof, MakeUndefinedOptional } from "./types.ts";
 
-type ExtractTagAttributes<TagDef> = TagDef extends { attributes?: infer A }
-  ? A extends BaseHTMLAttributeConfig
-    ? A
-    : {}
-  : {};
+// const comp = {
+//   tag: "a",
+//   attributes: {},
+//   innerHTML: {
+//     icon: {
+//       tag: "svg",
+//     },
+//     text: "",
+//   },
+//   css: {
+//     width: "100%",
+//     "> img": {},
+//     "> title": {},
+//   },
+// };
 
-type ExtractInnerHTML<TagDef> = TagDef extends { innerHTML?: infer I }
-  ? I
-  : never;
+type BaseComponentInnerHTMLStructure =
+  | Record<string, BaseComponentStructure | string>
+  | string;
 
-type AllowedChildTags<TagDef, AllTags extends string> =
-  ExtractInnerHTML<TagDef> extends "*"
-    ? AllTags
-    : ExtractInnerHTML<TagDef> extends readonly (infer T extends string)[]
-      ? Exclude<T, "#text">
-      : never;
-
-type AllowsText<TagDef> =
-  ExtractInnerHTML<TagDef> extends "*"
-    ? true
-    : ExtractInnerHTML<TagDef> extends readonly (infer T extends string)[]
-      ? "#text" extends T
-        ? true
-        : false
-      : false;
-
-type IsVoidInnerHTML<TagDef> =
-  ExtractInnerHTML<TagDef> extends readonly [] ? true : false;
-
-type CombinedAttributes<
-  TagDef,
-  GlobalAttrs extends BaseHTMLAttributeConfig,
-> = MakeUndefinedOptional<{
-  [K in
-    | keyof ExtractTagAttributes<TagDef>
-    | keyof GlobalAttrs]: K extends keyof ExtractTagAttributes<TagDef>
-    ? DSLInfer<ExtractTagAttributes<TagDef>[K] & string>
-    : K extends keyof GlobalAttrs
-      ? DSLInfer<GlobalAttrs[K] & string>
-      : never;
-}>;
-
-type ComponentConfig<
-  TagDefs extends BaseHTMLTagConfig,
-  GlobalAttrs extends BaseHTMLAttributeConfig,
-  AllowedTags extends string,
-> =
-  AllowedTags extends Keyof<TagDefs>
-    ? {
-        tag: AllowedTags;
-        attributes?: CombinedAttributes<TagDefs[AllowedTags], GlobalAttrs>;
-        innerHTML?: InnerHTMLValue<TagDefs, GlobalAttrs, TagDefs[AllowedTags]>;
-      }
-    : never;
-
-type NamedChildren<
-  TagDefs extends BaseHTMLTagConfig,
-  GlobalAttrs extends BaseHTMLAttributeConfig,
-  AllowedTags extends string,
-  TextAllowed extends boolean,
-> = {
-  [name: string]: NamedChildrenEntry<
-    TagDefs,
-    GlobalAttrs,
-    AllowedTags,
-    TextAllowed
-  >;
+type BaseComponentStructure = {
+  tag: string;
+  attributes?: Record<string, string>;
+  innerHTML?: BaseComponentInnerHTMLStructure;
+  css?: Record<string, any>;
 };
 
-type NamedChildrenEntry<
-  TagDefs extends BaseHTMLTagConfig,
-  GlobalAttrs extends BaseHTMLAttributeConfig,
-  AllowedTags extends string,
-  TextAllowed extends boolean,
-> = TextAllowed extends true
-  ?
-      | string
-      | ComponentConfig<TagDefs, GlobalAttrs, AllowedTags>
-      | ComponentConfig<TagDefs, GlobalAttrs, AllowedTags>[]
-  :
-      | ComponentConfig<TagDefs, GlobalAttrs, AllowedTags>
-      | ComponentConfig<TagDefs, GlobalAttrs, AllowedTags>[];
+type IsTextAllowed<
+  HTMLTagConfig extends BaseHTMLTagConfig,
+  Tag extends keyof HTMLTagConfig,
+> = "*" extends HTMLTagConfig[Tag]["innerHTML"]
+  ? true
+  : "#text" extends HTMLTagConfig[Tag]["innerHTML"][number]
+    ? true
+    : false;
 
-type InnerHTMLValue<
-  TagDefs extends BaseHTMLTagConfig,
-  GlobalAttrs extends BaseHTMLAttributeConfig,
-  TagDef,
-> =
-  IsVoidInnerHTML<TagDef> extends true
-    ? readonly never[]
-    : NamedChildren<
-        TagDefs,
-        GlobalAttrs,
-        AllowedChildTags<TagDef, Keyof<TagDefs>>,
-        AllowsText<TagDef>
-      >;
+type ValidateComponentInnerHTMLStructure<
+  HTMLAttributesConfig extends BaseHTMLAttributesConfig,
+  HTMLTagConfig extends BaseHTMLTagConfig,
+  CSSSyntaxConfig extends BaseCSSSyntaxConfig,
+  CSSAttributesConfig extends BaseCSSAttributesConfig,
+  CSSPropertiesConfig extends BaseCSSPropertiesConfig,
+  Tag extends keyof HTMLTagConfig,
+  T extends BaseComponentInnerHTMLStructure,
+> = Exclude<
+  {
+    [K in keyof T]: K extends string
+      ? T[K] extends BaseComponentStructure
+        ? [
+            Extract<
+              | HTMLTagConfig[Tag]["innerHTML"][number]
+              | HTMLTagConfig[Tag]["innerHTML"],
+              T[K]["tag"] | "*"
+            >,
+          ] extends [never]
+          ? { tag: Exclude<HTMLTagConfig[Tag]["innerHTML"][number], "#text"> }
+          : ValidateComponentStructure<
+              HTMLAttributesConfig,
+              HTMLTagConfig,
+              CSSSyntaxConfig,
+              CSSAttributesConfig,
+              CSSPropertiesConfig,
+              T[K]
+            >
+        : true extends IsTextAllowed<HTMLTagConfig, Tag>
+          ? T[K]
+          : never
+      : T[K];
+  },
+  true extends IsTextAllowed<HTMLTagConfig, Tag> ? never : string
+>;
+
+type ValidateComponentCSSStructure<
+  CSSSyntaxConfig extends BaseCSSSyntaxConfig,
+  CSSAttributesConfig extends BaseCSSAttributesConfig,
+  CSSPropertiesConfig extends BaseCSSPropertiesConfig,
+  T extends BaseComponentStructure,
+> = {
+  [K in keyof T["css"]]: K extends string
+    ? K extends keyof CSSAttributesConfig
+      ? DSLInfer<SupportedKeywords & CSSSyntaxConfig, CSSAttributesConfig[K]>
+      : `You need to have a valid css attribute`
+    : T["css"][K];
+};
+
+type ValidateComponentStructure<
+  HTMLAttributesConfig extends BaseHTMLAttributesConfig,
+  HTMLTagConfig extends BaseHTMLTagConfig,
+  CSSSyntaxConfig extends BaseCSSSyntaxConfig,
+  CSSAttributesConfig extends BaseCSSAttributesConfig,
+  CSSPropertiesConfig extends BaseCSSPropertiesConfig,
+  T extends BaseComponentStructure,
+> = {
+  [K in keyof T]: K extends string
+    ? K extends "tag"
+      ? Keyof<HTMLTagConfig>
+      : K extends "attributes"
+        ? MakeUndefinedOptional<
+            Partial<
+              InferHTMLAttributesConfig<SupportedKeywords, HTMLAttributesConfig>
+            > &
+              (HTMLTagConfig[T["tag"]]["attributes"] extends BaseHTMLAttributesConfig
+                ? InferHTMLAttributesConfig<
+                    SupportedKeywords,
+                    HTMLTagConfig[T["tag"]]["attributes"]
+                  >
+                : {})
+          >
+        : K extends "innerHTML"
+          ? T["innerHTML"] extends BaseComponentInnerHTMLStructure
+            ? ValidateComponentInnerHTMLStructure<
+                HTMLAttributesConfig,
+                HTMLTagConfig,
+                CSSSyntaxConfig,
+                CSSAttributesConfig,
+                CSSPropertiesConfig,
+                T["tag"],
+                T["innerHTML"]
+              >
+            : never
+          : K extends "css"
+            ? ValidateComponentCSSStructure<
+                CSSSyntaxConfig,
+                CSSAttributesConfig,
+                CSSPropertiesConfig,
+                T
+              >
+            : null
+    : T[K];
+};
 
 export function createComponent<
-  const TagDefs extends BaseHTMLTagConfig,
-  const GlobalAttrs extends BaseHTMLAttributeConfig,
+  const HTMLAttributesConfig extends BaseHTMLAttributesConfig,
+  const HTMLTagConfig extends BaseHTMLTagConfig,
+  const CSSSyntaxConfig extends BaseCSSSyntaxConfig,
+  const CSSAttributesConfig extends BaseCSSAttributesConfig,
+  const CSSPropertiesConfig extends BaseCSSPropertiesConfig,
+  const T extends BaseComponentStructure,
 >(
-  _tagDefs: TagDefs,
-  _globalAttrs: GlobalAttrs,
-  config: ComponentConfig<TagDefs, GlobalAttrs, Keyof<TagDefs>>,
+  _HTMLAttributesConfig: HTMLAttributesConfig,
+  _HTMLTagConfig: HTMLTagConfig,
+  _CSSSyntaxConfig: CSSSyntaxConfig,
+  _CSSAttributesConfig: CSSAttributesConfig,
+  _CSSPropertiesConfig: CSSPropertiesConfig,
+  config: ValidateComponentStructure<
+    HTMLAttributesConfig,
+    HTMLTagConfig,
+    CSSSyntaxConfig,
+    CSSAttributesConfig,
+    CSSPropertiesConfig,
+    T
+  >,
 ) {
-  return config;
+  return config as T;
 }
